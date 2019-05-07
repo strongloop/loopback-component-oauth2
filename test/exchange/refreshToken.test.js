e7c90e049ed29507477b250167a039b8f6bd81e3
Agnes Lin <agneslin.lin@ibm.com>
2019-05-07 09:33:34 -0400

    chore: update copyrights years

--- test/exchange/refreshToken.test.js
+++ test/exchange/refreshToken.test.js
@@ -1,4 +1,4 @@
-// Copyright IBM Corp. 2013,2016. All Rights Reserved.
+// Copyright IBM Corp. 2013,2017. All Rights Reserved.
 // Node module: loopback-component-oauth2
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
